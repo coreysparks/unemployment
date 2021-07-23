45eef72ccad3530b5b5f8b542e2b784fd08d5c44
Corey Sparks <corey.sparks@utsa.edu>
2021-07-23 10:00:32 -0500

    updated through 7/23/21

--- make_datafiles.R
+++ make_datafiles.R
@@ -1,7 +1,7 @@
 library(haven)
 library(ipumsr)
 
-ddi<- read_ipums_ddi(ddi_file = "~/OneDrive - University of Texas at San Antonio/projects/carsey_brief/cps_00037.xml")
+ddi<- read_ipums_ddi(ddi_file = "~/OneDrive - University of Texas at San Antonio/projects/carsey_brief/cps_00039.xml")
 cpsdat<-read_ipums_micro(ddi)
 cpsdat<-zap_labels(cpsdat)
 save(cpsdat, file="~/OneDrive - University of Texas at San Antonio/projects/carsey_brief/cpsbriefdata.Rdata")
